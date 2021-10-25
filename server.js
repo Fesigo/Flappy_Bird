bf5cbddcf84e4be6f8665c7187c9c17a95aad1fb
felipe.siqueira <felipesiqgodoy@gmail.com>
2021-10-24 22:13:54 -0300

    documented the code

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const path = require('path');
 const http = require('http');
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); // creating server
 
-var WebSocketServer = require('websocket').server;
+var WebSocketServer = require('websocket').server; // creating websocket server
 
 app.use(express.static(__dirname));
 
-app.get('/screenleft', (req, res) => {
+// route for the left screen
+app.get('/screenleft', (req, res) => { 
     res.sendFile(path.join(__dirname, '/index.html'));
 })
 
+// route for the right screen
 app.get('/screenright', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 })
@@ -21,16 +23,18 @@ const port = 3003;
 
 server.listen(port, () => console.log(`Server started on port ${port}`));
 
+
 wsServer = new WebSocketServer({
     httpServer: server
 });
 
-const clients = [];
+const clients = []; // array of clients connected
 
 wsServer.on('request', (request) => {
-    const connection = request.accept(null, request.origin)
+    const connection = request.accept(null, request.origin) // accepting new connection
     clients.push(connection);
     
+    // sending messages from the server to all clients connected
     connection.on('message', message => {
         // console.log(message.utf8Data);
         clients.forEach(c => {
@@ -38,6 +42,7 @@ wsServer.on('request', (request) => {
         })
     })
 
+    // log message when connections end
     connection.on('close', message => {
         console.log('Closed Connection');
     })
--- src/jogo.js
+++ src/jogo.js
@@ -1,4 +1,4 @@
-let frames = 0;
+let frames = 0; // frame counter
 
 const hitSound = new Audio();
 hitSound.src = './src/assets/sounds/hit.wav';
@@ -19,11 +19,13 @@ const contexto = canvas.getContext('2d');
 canvas.width = 1366;
 canvas.height = 657;
 
+// client connected
 const client = new WebSocket('ws://localhost:3003');
 client.onopen = function() {
     console.log('WebSocket working');
 }
 
+//function to check the collision between the bird and the land
 function collide(flappyBird, land) {
 
     const birdY = flappyBird.y + flappyBird.altura;
@@ -37,6 +39,7 @@ function collide(flappyBird, land) {
 
 }
 
+// function to create a flappybird object
 function createFlappyBird() {
 
     // Bird
@@ -48,6 +51,8 @@ function createFlappyBird() {
         x: 250,
         y: 200,
         pulo: 4.6,
+
+        // function that implements the bird's jump
         jump() {
             jumpSound.play();
             flappyBird.velocidade = -flappyBird.pulo;
@@ -55,7 +60,11 @@ function createFlappyBird() {
         },
         gravidade: 0.25,
         velocidade: 0,
+
+        // function to update the bird's Y
         atualiza() {
+
+            // checking collision
             if (collide(flappyBird, globais.land)) {
 
                 hitSound.play();
@@ -72,13 +81,17 @@ function createFlappyBird() {
             flappyBird.velocidade += flappyBird.gravidade;
             flappyBird.y += flappyBird.velocidade;
         },
+
+        // array of sprites of the bird in different positions to make the flying animation
         movimentos: [
-            { spriteX: 0,  spriteY: 0 }, // asa para cima
-            { spriteX: 0,  spriteY: 24 }, // asa no meio
-            { spriteX: 0,  spriteY:48 }, // asa para baixo
-            { spriteX: 0,  spriteY: 24 }, // asa no meio
+            { spriteX: 0,  spriteY: 0 }, // wings up
+            { spriteX: 0,  spriteY: 24 }, // wings in the middle
+            { spriteX: 0,  spriteY:48 }, // wings down
+            { spriteX: 0,  spriteY: 24 }, // wings in the middle
         ],
         currentFrame: 0,
+
+        // changing the bird's sprite according to the frames
         updateCurrentFrame() {
 
             const framesInterval = 10;
@@ -92,15 +105,17 @@ function createFlappyBird() {
 
 
         },
+
+        // function to draw the bird in the canvas
         desenha() {
             flappyBird.updateCurrentFrame();
             const { spriteX, spriteY } = this.movimentos[flappyBird.currentFrame];
             contexto.drawImage(
                 bird,
-                spriteX, spriteY, // Sprite X, Sprite Y
-                flappyBird.largura, flappyBird.altura,  // Tamanho do recorte na sprite
-                flappyBird.x, flappyBird.y,             // posição do pássaro no canvas
-                flappyBird.largura, flappyBird.altura   // tamanho do pássaro no canvas
+                spriteX, spriteY,
+                flappyBird.largura, flappyBird.altura,
+                flappyBird.x, flappyBird.y,
+                flappyBird.largura, flappyBird.altura
             );
         }
     }
@@ -109,8 +124,9 @@ function createFlappyBird() {
 
 }
 
-// Land
+// function to create a land object
 function createLand() {
+    // Land
     const chao = new Image();
     chao.src = './src/assets/imgs/land.png'
     const land = {
@@ -120,6 +136,8 @@ function createLand() {
         altura: 112,
         x: 0,
         y: canvas.height -112,
+
+        // function to update the land's X, making the animation
         atualiza() {
             const landMovement = 1;
 
@@ -129,6 +147,8 @@ function createLand() {
 
             land.x = movement % repeat;
         },
+
+        // function to draw the land in the canvas
         desenha() {
             for(let i = 0; i * land.largura <= canvas.width * 2; i++) {
                 contexto.drawImage(
@@ -145,6 +165,7 @@ function createLand() {
     return land;
 }
 
+// function to create pipes objects
 function createPipes() {
     const pipes = {
         largura: 52,
@@ -158,6 +179,8 @@ function createPipes() {
             spriteY: 169
         },
         spaceBetween: 80,
+
+        // function to draw the pairs of pipes in the canvas
         desenha() {
             
             pipes.pairs.forEach(pair => {
@@ -167,7 +190,7 @@ function createPipes() {
                 const skyPipeX = pair.x;
                 const skyPipeY = yRandom;
 
-                // Cano de cima
+                // top pipe
                 contexto.drawImage(
                     sprites,
                     pipes.sky.spriteX, pipes.sky.spriteY,
@@ -179,7 +202,7 @@ function createPipes() {
                 const groundPipeX = pair.x;
                 const groundPipeY = pipes.altura + spacementBetweenPipes + yRandom;
     
-                // Cano de baixo
+                // bottom pipe
                 contexto.drawImage(
                     sprites,
                     pipes.ground.spriteX, pipes.ground.spriteY,
@@ -188,6 +211,7 @@ function createPipes() {
                     pipes.largura, pipes.altura
                 )
 
+                // adjusting the height of the pipes according to the ground and the sky
                 pair.skyPipe = {
                     x: skyPipeX,
                     y: pipes.altura + skyPipeY
@@ -199,6 +223,7 @@ function createPipes() {
             })
         },
 
+        // function to check the collision between the bird and the pipes
         hasCollisionWithBird(pair) {
 
             const flappyHead = globais.flappyBird.y;
@@ -218,8 +243,8 @@ function createPipes() {
             return false;
         },
 
-        pairs: [],
-        pairsAux: [
+        pairs: [], // array for the pipes that will be draw
+        pairsAux: [ // predefined array of pipes
             {
                 x: null,
                 y: -225
@@ -269,22 +294,26 @@ function createPipes() {
                 y: -125
             }
         ],
+
+        // function to update the position of the pipes
         atualiza() {
             const passed100Frames= frames % 100 == 0;
-            if (passed100Frames) {
+            if (passed100Frames) { // drawing nem pair of pipes every 100 frames
 
-                pipeAux = this.pairsAux.shift();
-                pipeAux.x = window.location.pathname === '/screenright' ? canvas.width : canvas.width * 2 + 100
-                pipes.pairs.push(pipeAux);
-                this.pairsAux.push({
+                pipeAux = this.pairsAux.shift(); // removing the first pipe of the predefined array
+                pipeAux.x = window.location.pathname === '/screenright' ? canvas.width : canvas.width * 2 + 100 // setting its X position according to the screen that is being used
+                pipes.pairs.push(pipeAux); // pushing the pair of pipe into the pairs array, that will be used to draw the pipes
+                this.pairsAux.push({ // pushing the removed pair back at the end of the predefined array
                     x: null,
                     y: pipeAux.y
                 });
             }
 
+            // changing the pairs X position / animating the pipes
             pipes.pairs.forEach(pair => {
                 pair.x -= 2;
 
+                // checking collision between the bird and the pipe
                 if (pipes.hasCollisionWithBird(pair)) {
                     hitSound.play();
                     setTimeout(() => {
@@ -294,9 +323,9 @@ function createPipes() {
                     // changeToScreen(screens.gameOver);
                 }
 
-                if(pair.x + pipes.largura <= 0) {
-                    pipes.pairs.shift();
-                }
+                // if(pair.x + pipes.largura <= 0) {
+                //     pipes.pairs.shift();
+                // }
             })
 
         }
@@ -306,11 +335,13 @@ function createPipes() {
     return pipes;
 }
 
+// function to create the score object
 function createScore() {
 
     const score = {
         points: 0,
 
+        // function to draw the score in the canvas
         desenha() {
             contexto.font = '60px "VT323"';
             contexto.textAlign = 'right'
@@ -319,6 +350,7 @@ function createScore() {
             contexto.fillText(`${score.points}`, canvas.width - 50, 70);
         },
 
+        // function to update the score value
         atualiza() {
             const framesInterval = 85;
 
@@ -342,6 +374,8 @@ const background = {
     altura: 109,
     x: 0,
     y: canvas.height - 221,
+
+    // drawing the background in the canvas
     desenha() {
 
         contexto.fillStyle = '#4ec0ca';
@@ -369,6 +403,8 @@ const getReadyScreen = {
     h: 170,
     x: (canvas.width / 2) - 188 / 2,
     y: 100,
+
+    // drawing the initial screen in the canvas
     desenha() {
         contexto.drawImage(
             getReady,
@@ -380,7 +416,7 @@ const getReadyScreen = {
     }
 }
 
-// Game over screen
+// Game over screen (WIP)
 const gameOverMessage = {
     sX: 134,
     sY: 153,
@@ -388,6 +424,8 @@ const gameOverMessage = {
     h: 200,
     x: (canvas.width / 2) - 226 / 2,
     y: 100,
+
+    // drawing the game over message in the canvas
     desenha() {
         contexto.drawImage(
             sprites,
@@ -403,8 +441,10 @@ const gameOverMessage = {
  * Telas
 */
 
-const globais = {};
-let activeScreen = {};
+const globais = {}; // object the keeps all the other objects created
+let activeScreen = {}; // object that defines the active screen in the game
+
+// function to change between the screens
 function changeToScreen(newScreen) {
     activeScreen = newScreen;
 
@@ -413,9 +453,12 @@ function changeToScreen(newScreen) {
     }
 }
 
+// screens of the game
 const screens = {
     
+    // initial screen
     inicio: {
+        // function to the objects it needs in the begging
         inicializa() {
             globais.flappyBird = createFlappyBird();
             globais.land = createLand();
@@ -426,12 +469,15 @@ const screens = {
                 globais.pipes.x = canvas.width * 2
             }
         },
+
+        // function to draw the objects created
         desenha() {
             background.desenha();
             globais.flappyBird.desenha();
             globais.land.desenha();
             getReadyScreen.desenha();
 
+            // positioning the getReadyScreen according to the browser's window
             if(window.location.pathname === '/screenright') {
                 getReadyScreen.x = ((canvas.width / 2) - 188 / 2) - canvas.width / 2;
             }
@@ -443,33 +489,45 @@ const screens = {
             //     globais.flappyBird.x -= canvas.width
             // }
         },
+
+        // function to change from the initial screen to the game's screen when clicked
         click() {
             changeToScreen(screens.jogo);
         },
+
+        // updating the ground X position
         atualiza() {
             globais.land.atualiza();
         },
         initialScreen: true
     },
 
+    // game screen
     jogo: {
+        // creating the score
         inicializa() {
             globais.score = createScore();
         },
+
+        // drawing the games objects
         desenha() {
             background.desenha();
             globais.pipes.desenha();
             globais.land.desenha();
             globais.flappyBird.desenha();
             
+            // positioning the bird and the score according to the browser's window
             if(window.location.pathname === '/screenright') {
                 globais.score.desenha();
                 globais.flappyBird.x -= canvas.width
             }
         },
+        // function to make the bird jump when clicked
         click() {
             globais.flappyBird.jump();
         },
+
+        // updating the game's objects
         atualiza() {
             globais.pipes.atualiza();
             globais.land.atualiza();
@@ -478,48 +536,58 @@ const screens = {
         }
     },
 
+    // game over screen (WIP)
     gameOver: {
+        // drawing the game over screen
         desenha() {
             gameOverMessage.desenha();
         },
         atualiza() {
 
         },
+        // changing to the initial screen when clicked
         click() {
             changeToScreen(screens.inicio);
         }
     }
 }
 
+// game loop
 function loop() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
+    // executing the function that draws the objects needed in the active screen
     activeScreen.desenha();
+    // executing the function that updates the objects in the active screen
     activeScreen.atualiza();
     
-    frames++;
+    frames++; // incrementing frame counter
     requestAnimationFrame(loop);
 }
 
+// sending flappyBird.velocidade through websocket connection
 function sendMessage() {
     client.send(globais.flappyBird.velocidade);
 }
 
+
 window.addEventListener('click', () => {
+    // checks if the current screen has a click function and the executes it
     if (activeScreen.click()) {
         activeScreen.click();
     }
     sendMessage();
 });
 
-changeToScreen(screens.inicio);
+changeToScreen(screens.inicio); // beggining with the initial screen
 
-loop();
+loop(); // starting the loop
 
+// receiving messages from the server
 client.onmessage = event => {
     // console.log(event.data);
-    if (activeScreen.initialScreen) changeToScreen(screens.jogo);
-    if (event.data == 'died') changeToScreen(screens.inicio);
-    globais.flappyBird.velocidade = -globais.flappyBird.pulo;
+    if (activeScreen.initialScreen) changeToScreen(screens.jogo); // changing from the initial screen to the game screen in all clients connected
+    if (event.data == 'died') changeToScreen(screens.inicio); // changing from the game screen to the initial screen in all clients connected, when the bird dies
+    globais.flappyBird.velocidade = -globais.flappyBird.pulo; // updating the bird's Y position in all clients connected
 }
